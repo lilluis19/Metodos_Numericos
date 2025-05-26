107ddb91b5503b2a75555d03c52f08e2b79c6006
lilluis19 <23030157@itesa.edu.mx>
2025-05-26 12:23:18 -0600

    Update Ejemplo1.java

--- Tema1/Error Absoluto/Ejemplo1.java
+++ Tema1/Error Absoluto/Ejemplo1.java
@@ -1,1 +1,13 @@
 
+package errorabsoluto;
+
+public class ErrorAbsoluto {
+
+    public static void main(String[] args) {
+        double valorReal = 5.678;
+        double valorAproximado = 5.65;
+        double errorAbsoluto = Math.abs(valorReal - valorAproximado);
+        System.out.println("Error Absoluto: " + errorAbsoluto);
+    }   
+}
+
