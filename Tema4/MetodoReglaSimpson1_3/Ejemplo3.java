f4023390fd416d5d54ee9325a813d08e3300a50a
lilluis19 <23030157@itesa.edu.mx>
2025-05-28 00:23:32 -0600

    Create Ejemplo3.java

---
+++ Tema4/MetodoReglaSimpson1_3/Ejemplo3.java
@@ -0,0 +1,37 @@
+package Tema4.MetodoReglaSimpson1_3;
+
+import java.util.function.Function;
+
+public class Ejercicio3 {
+    public static double simpsonOneThirdRule(Function<Double, Double> f, double a, double b, int n) {
+        double h = (b - a) / n;
+        double sum = f.apply(a) + f.apply(b);
+
+        for (int i = 1; i < n; i++) {
+            double x = a + i * h;
+            if (i % 2 == 0) {
+                sum += 2 * f.apply(x);
+            } else {
+                sum += 4 * f.apply(x);
+            }
+        }
+
+        return (h / 3) * sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Método de la Regla de Simpson 1/3 - Ejercicio 3");
+
+        Function<Double, Double> f = x -> Math.exp(-x*x);
+        double a = -1;
+        double b = 1;
+        int n = 6;
+        if (n % 2 != 0) {
+            System.out.println("El número de subintervalos debe ser par.");
+            return;
+        }
+
+        double result = simpsonOneThirdRule(f, a, b, n);
+        System.out.println("Integral de e^(-x²) entre -1 y 1: " + result);
+    }
+}
