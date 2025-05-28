55d0943a4e08af4e90920a45abe2278315543b5f
lilluis19 <23030157@itesa.edu.mx>
2025-05-28 01:12:05 -0600

    Create Ejemplo5.java

---
+++ Tema6/Euler/Ejemplo5.java
@@ -0,0 +1,27 @@
+package Tema6.Euler;
+
+public class Ejercicio5 {
+    // Ecuación diferencial: dy/dx = y - cos(x)
+    public static double f(double x, double y) {
+        return y - Math.cos(x);
+    }
+
+    public static void metodoEuler(double x0, double y0, double h, int n) {
+        double x = x0;
+        double y = y0;
+
+        System.out.printf("x0: %.4f, y0: %.4f%n", x, y);
+
+        for (int i = 0; i < n; i++) {
+            y = y + h * f(x, y);
+            x = x + h;
+            System.out.printf("x%d: %.4f, y%d: %.4f%n", i + 1, x, i + 1, y);
+        }
+    }
+
+    public static void main(String[] args) {
+        double x0 = 0.0, y0 = 0.0, h = 0.15;
+        int n = 10;
+        metodoEuler(x0, y0, h, n);
+    }
+}
