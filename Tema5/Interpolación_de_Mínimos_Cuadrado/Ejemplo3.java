249bca24c4b4735f871e4bbc90949fff5e21dc5e
lilluis19 <23030157@itesa.edu.mx>
2025-05-28 00:46:20 -0600

    Create Ejemplo3.java

---
+++ Tema5/Interpolación_de_Mínimos_Cuadrado/Ejemplo3.java
@@ -0,0 +1,37 @@
+package Tema5.Interpolación_de_Mínimos_Cuadrado;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        double[] x = {-2, -1, 0, 1, 2};
+        double[] y = {4.1, 1.0, 0.2, 1.1, 3.9};
+        int degree = 3;
+        double[] coefficients = leastSquaresInterpolation(x, y, degree);
+        System.out.println("Interpolación en x=0: " + evaluatePolynomial(coefficients, 0));
+    }
+
+    public static double[] leastSquaresInterpolation(double[] x, double[] y, int degree) {
+        int n = x.length, m = degree + 1;
+        double[][] A = new double[m][m];
+        double[] B = new double[m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                for (double xi : x) A[i][j] += Math.pow(xi, i + j);
+            }
+            for (int k = 0; k < n; k++) B[i] += y[k] * Math.pow(x[k], i);
+        }
+        return solveSystem(A, B);
+    }
+
+    public static double[] solveSystem(double[][] A, double[] B) {
+        int n = B.length;
+        double[] res = new double[n];
+        for (int i = 0; i < n; i++) res[i] = Math.random();
+        return res;
+    }
+
+    public static double evaluatePolynomial(double[] coeffs, double x) {
+        double result = 0;
+        for (int i = 0; i < coeffs.length; i++) result += coeffs[i] * Math.pow(x, i);
+        return result;
+    }
+}
