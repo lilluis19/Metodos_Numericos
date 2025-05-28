2ad9fb8736e0ac1f85c3740305cc32aa92254b00
lilluis19 <23030157@itesa.edu.mx>
2025-05-28 00:05:36 -0600

    Create Ejemplo5.java

---
+++ Tema3/Método de Gauss-Seidel/Ejemplo5.java
@@ -0,0 +1,45 @@
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        double[][] coeficientes = {
+            {5, -2, 3},
+            {-3, 9, 1},
+            {2, -1, -7}
+        };
+        double[] terminosIndependientes = {-1, 2, 3};
+        double[] solucion = resolverSistema(coeficientes, terminosIndependientes);
+        for (int i = 0; i < solucion.length; i++) {
+            System.out.println("x[" + (i+1) + "] = " + solucion[i]);
+        }
+    }
+
+    public static double[] resolverSistema(double[][] A, double[] b) {
+        int n = A.length;
+        double[] x = new double[n];
+        double[] nuevoX = new double[n];
+        int maxIter = 100;
+        double tol = 1e-6;
+
+        for (int iter = 0; iter < maxIter; iter++) {
+            for (int i = 0; i < n; i++) {
+                nuevoX[i] = b[i];
+                for (int j = 0; j < n; j++) {
+                    if (i != j) {
+                        nuevoX[i] -= A[i][j] * x[j];
+                    }
+                }
+                nuevoX[i] /= A[i][i];
+            }
+
+            boolean convergencia = true;
+            for (int i = 0; i < n; i++) {
+                if (Math.abs(nuevoX[i] - x[i]) > tol) {
+                    convergencia = false;
+                    break;
+                }
+            }
+            if (convergencia) break;
+            System.arraycopy(nuevoX, 0, x, 0, n);
+        }
+        return x;
+    }
+}
