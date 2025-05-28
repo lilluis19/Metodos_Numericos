b8947628bde42094b6d36d7dd832aad59c32d207
lilluis19 <23030157@itesa.edu.mx>
2025-05-27 22:09:43 -0600

    Create Ejemplo5.java

---
+++ Tema1/Cancelación numérica/Ejemplo5.java
@@ -0,0 +1,12 @@
+package cancelacionnumerica;
+
+public class CancelacionNumerica4 {
+
+    public static void main(String[] args) {
+        double a = 123456789.123456789;
+        double b = 123456789.123456788;
+        double resultado = a - b;
+
+        System.out.println("Resultado de la Cancelación Numérica 4: " + resultado);
+    }
+}
