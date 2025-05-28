ae83aa812b85cb58724c25940bb7f92e55a4be5a
lilluis19 <23030157@itesa.edu.mx>
2025-05-27 22:28:16 -0600

    Create Ejemplo2.java

---
+++ Tema1/Error numérico total/Ejemplo2.java
@@ -0,0 +1,18 @@
+package errornumericototal;
+
+public class ErrorNumericoTotal1 {
+
+    public static void main(String[] args) {
+        double x = Math.PI / 6;
+        double valorReal = Math.sin(x);
+        double valorAproximado = x - Math.pow(x, 3) / 6;
+
+        valorAproximado = Math.round(valorAproximado * 10000.0) / 10000.0;
+
+        double errorTruncamiento = Math.abs(valorReal - valorAproximado);
+        double errorRedondeo = Math.abs(valorReal - (Math.sin(x) - errorTruncamiento));
+        double errorNumericoTotal = errorTruncamiento + errorRedondeo;
+
+        System.out.println("Error Numérico Total 1: " + errorNumericoTotal);
+    }
+}
