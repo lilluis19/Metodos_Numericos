51230221ca1f2ad35fa2881900e14362472a5495
lilluis19 <23030157@itesa.edu.mx>
2025-05-27 22:24:20 -0600

    Create Ejemplo3.java

---
+++ Tema1/Error de truncamieinto/Ejemplo3.java
@@ -0,0 +1,13 @@
+package errordetruncamiento;
+
+public class ErrorDeTruncamiento2 {
+
+    public static void main(String[] args) {
+        double x = 1.5;
+        double valorReal = Math.exp(x);
+        double valorAproximado = 1 + x + (Math.pow(x, 2) / 2);
+        double errorTruncamiento = Math.abs(valorReal - valorAproximado);
+
+        System.out.println("Error de Truncamiento 2: " + errorTruncamiento);
+    }
+}
